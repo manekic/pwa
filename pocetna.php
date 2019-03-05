cb419a57f5e320b629d2e9f6fc7d6fb6e2dfa930
manekic <maja_nekic@hotmail.com>
2019-03-05 15:10:09 +0100

    komentari

--- pocetna.php
+++ pocetna.php
@@ -7,12 +7,12 @@ $_GET['pass'] = pass korisnika
 
 OUTPUT: JSON
 {
-  uspjeh = varijabla koja kaze je li prijava uspjela
+  info = varijabla koja kaze je li prijava uspjela
   rez = rezultati ispita dostupni u bazi
 }
 ili
 {
-  error = poruka o greski.
+  error = poruka o greški.
 }
 */
 
@@ -30,25 +30,27 @@ function sendErrorAndExit($messageText)
 	$message['error'] = $messageText;
 	sendJSONandExit($message);
 }
-//  1.UPIT
+//ako ajaxom šaljemo username && pass
 if(isset($_GET['username']) && isset($_GET['pass'])) {
   $username = $_GET['username'];
   $pass = $_GET['pass'];
   $message = [];
+  $message['info'] = [];
   $message['rez'] = [];
-  $message['nazivi'] = [];
+  //kontrolna varijabla za prijavu
   $flag = false;
   //polje za nazive kolegija
   $kolegiji = array();
   $br = 0;
-  //spajanje na bazu
+  //spajanje na bazu, tablica studenti
   try {
       $db = DB::getConnection();
       $st = $db->prepare('SELECT student_id, username, password, ime, prezime FROM studenti');
       $st->execute();
     }
     catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
-
+  //dohvaćanje svih podataka korisnika koji se prijavio
+  //ako proslijeđeni username ne postoji u bazi -> krivo korisničko ime!
   while($row = $st->fetch()) {
     if($row['username'] === $username) {
       $id = $row['student_id'];
@@ -58,48 +60,45 @@ if(isset($_GET['username']) && isset($_GET['pass'])) {
       $flag = true;
     }
   }
-
+  //spajanje na bazu, tablica kolegiji
   try {
       $db = DB::getConnection();
       $st1 = $db->query('SELECT naziv_kolegija FROM kolegiji');
     }
     catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
-
-
   //popunjavanje polja sa nazivima kolegija
   foreach($st1->fetchAll() as $row1) {
     $kolegiji[$br++] = $row1['naziv_kolegija'];
-    //$message['nazivi'][] = $row1['naziv_kolegija'];
   }
   //ako ne postoji redak u kojemu je student_id == $username
   if($flag === false) {
-    $message['uspjeh'] = "Ne postoji korisnik sa unesenim korisnickim imenom.";
+    $message['info'] = "Ne postoji korisnik sa unesenim korisnickim imenom.";
   }
   //ako username postoji u bazi, ali se $pass i lozinka u tablici ne podudaraju
   else if($pass !== $pass_studenta){
-    $message['uspjeh'] = "Lozinka nije ispravna.";
+    $message['info'] = "Lozinka nije ispravna.";
   }
   //ako je sve u redu, iz baze dohvacamo rezultate svih kolegija koje je student upisao
   else {
-    $message['uspjeh'] = "Sve OK.";
+    $message['info'] = "Sve OK.";
+    //spajanje na bazu, tablica rezultati
     try {
         $db = DB::getConnection();
         $st2 = $db->query('SELECT * FROM rezultati');
       }
       catch( PDOException $e ) { exit( 'PDO error ' . $e->getMessage() ); }
-
+    //podatke iz baze zapisujem u povratnu varijablu
     foreach($st2->fetchAll() as $row2) {
       if($row2['student_id'] === $id)
       {
-        $message['nazivi'][] = $kolegiji[$row2['kolegij_id']-1];
+        //$message['nazivi'][] = $kolegiji[$row2['kolegij_id']-1];
         $message['rez'][] = array('ime' => $kolegiji[$row2['kolegij_id']-1], 'kol1' => $row2['1kolokvij'],
                                   'kol2' => $row2['2kolokvij'], 'zavrsni' => $row2['zavrsni'], 'dz1' => $row2['1zadaca'],
                                   'dz2' => $row2['2zadaca'], 'dz3' => $row2['3zadaca'],'dz4' => $row2['4zadaca']);
       }
     }
-
-
   }
+  //slanje povratnih podataka
   sendJSONandExit($message);
 }
 else
